0058ad399a18697a9f2c3287fdd52a31d4ac87b9
akloya <ajaykumar.loya@gmail.com>
2019-01-23 22:27:50 -0800

    Update slack.py

--- common/slack.py
+++ common/slack.py
@@ -1,1 +1,21 @@
-abc
+
+import json
+import requests
+
+webhook = "https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxx"
+
+def send_slack_message(color, pretext, title, message):
+    payload = {
+        "attachments": [
+            {
+                "color": color,
+                "pretext": pretext,
+                "title": title,
+                "text": message,
+                "footer": "AWS AUTOMATION ALERTS"
+            }
+        ]
+    }
+    requests.post(webhook,
+                  json.dumps(payload),
+                  headers={'content-type': 'application/json'})
